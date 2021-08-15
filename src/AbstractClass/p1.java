2f55c975188b702ef9ddbe964447194c4e0aa716
pradyumna <pradyumna.murali@gmail.com>
2021-08-15 11:46:02 +0530

    test1

--- src/AbstractClass/p1.java
+++ src/AbstractClass/p1.java
@@ -6,5 +6,6 @@ public abstract class p1 {
     public static void main(String[] args)
     {
     	//p1 ob1 = new p1(); >>>> we cannot create object for an abstract class : abstract class cannot be instantiated.
+    	System.out.println ("test 1");
     }
 }
